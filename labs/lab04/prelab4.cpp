dcb12d576a0b4f2f2495e86e2332ca0f9f2489dc
Joshua Tapp <joshua.c.tapp@icloud.com>
2021-03-07 10:03:15 -0500

    Finished prelab4 and inlab4 for lab04.

---
+++ labs/lab04/prelab4.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+
+void sizeOfTest() {
+   cout << "Size of int: " << sizeof(int) << endl;
+   cout << "Size of unsigned int: " << sizeof(unsigned int) << endl;
+   cout << "Size of float:" << sizeof(float) << endl;
+   cout << "Size of double: " << sizeof(double) << endl;
+   cout << "Size of char:" << sizeof(char) << endl;
+   cout << "Size of bool: " << sizeof(bool) << endl;
+   cout << "Size of int*: " << sizeof(int*) << endl;
+   cout << "Size of char*: " << sizeof(char*) << endl;
+   cout << "Size of double*: " << sizeof(double*) << endl;
+}
+
+
+void overflow() {
+    unsigned int i = UINT_MAX;
+    i++;
+    cout << UINT_MAX << " + 1 = " << i << endl;
+}
+
+void outputBinary(unsigned int x) {
+    unsigned int temp = x;
+    int power = 31;
+    int div   = 0;
+    for(int i = 0; i < 8; i++) {
+        for(int j = 0; j < 4; j++) {
+            div = pow(2, power);
+            if(temp >= div) {
+                cout << "1";
+                temp = temp - div;
+            }
+            else {
+                cout << "0";
+            }
+            power = power - 1;
+        }
+        cout << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+  sizeOfTest();
+  overflow();
+  unsigned int l;
+  cin >> l;
+  outputBinary(l);
+
+  return 0;
+}
