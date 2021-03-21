e2b60b7d6bdc3fb9a2e627527187812cba45d8bc
Joshua Tapp <joshua.c.tapp@icloud.com>
2021-03-21 14:09:52 -0400

    Inlab compiling. Fresh Start.

--- labs/lab05/code/inlab/BinarySearchTree.cpp
+++ labs/lab05/code/inlab/BinarySearchTree.cpp
@@ -46,6 +46,13 @@ BinaryNode* BinarySearchTree::insert(BinaryNode*& n, const string& x) {
     else{}
     return n; // return the tree/branch if we arnt creating new root.
 }
+BinaryNode* BinarySearchTree::insert(BinaryNode*& n, const string& x) {
+    if (n == NULL) {
+        BinaryNode* node = new BinaryNode();
+        node->value = x;
+        return node;
+    }
+}
 
 // remove finds x's position in the tree and removes it.
 void BinarySearchTree::remove(const string& x) {
