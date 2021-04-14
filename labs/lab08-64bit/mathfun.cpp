28e6979a5c2ca25359d75da5fcbecedbc0c11299
Joshua Tapp <joshua.c.tapp@icloud.com>
2021-04-13 21:43:42 -0400

    fatal error
    fatal error: 'wchar.h' file not found
    #include_next <wchar.h>
    
    Have to find the fix for this.

---
+++ labs/lab08-64bit/mathfun.cpp
@@ -0,0 +1,20 @@
+// Joshua Tapp
+// UVA CS2150
+// 4/13/2021
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+extern "C" long product (long a, long b);
+extern "C" long power (long a, long b);
+
+int main() {
+    long x, y;
+    cout << "Enter integer 1: ";
+    cin >> x;
+    cout << "\nEnter integer 2: ";
+    cin >> y;
+    cout << "\nProduct: " << product(x,y) << "\nPower: " << power(x,y) << endl;
+    return 0;
+}
