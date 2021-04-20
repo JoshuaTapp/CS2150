ce91d78bc189a04e56e917c237532bdf5bc767e4
Joshua Tapp <joshua.c.tapp@icloud.com>
2021-04-20 18:50:06 -0400

    postlab09 finished
    Went with regular old binary search. Sad I couldnt figure out how to do boundless, im sure it was something simple. This was 100% written by me without reference.

--- labs/lab09-64bit/postlab/binarySearch.s
+++ labs/lab09-64bit/postlab/binarySearch.s
@@ -1,23 +1,27 @@
-; Joshua Tapp
+; Joshua Tapp 
+; UVA CS2150 SPR21
+; Lab09 Postlab
+; This code was written without any reference besides this: https://www.hackerearth.com/practice/algorithms/searching/binary-search/tutorial/
+; There was zero use of Godbolt. 
 
-global _binarySearch
+global binarySearch
 section .text
 
 ;   rdi : arr pointer   (64 bit pointer)
 ;   rsi : low           (32 bit integer)
 ;   rdx : high          (32 bit integer)
 ;   rcx : target        (32 bit integer)
+;   rbx : mid           (32 bit integer) - using this for variable of mid value.
 ;   List is sorted, low = index[start], high = index[size - 1], target = search value 
 
-; NOTE: we are using a boundless binary search algorithm. Source: https://github.com/scandum/binary_search/blob/master/binary_search.c
-;       I DID NOT DUMP THIS INTO GODBOLT! This is my code alone.
 
-_binarySearch:
+binarySearch:
         push    rcx             ; we will be modding these, so push to stack for safety.
         push    rdx
         push    rsi
         push    rdi
         push    rbx
+        push    r8
 
         mov     rax, -1         ; This section will test if the input data is bad.
         cmp     edx, -1         ; Which means that size = 0
@@ -25,41 +29,35 @@ _binarySearch:
         xor     rax, rax        ; zero out return register
 
 _search_loop:
-        cmp     edx, 1          ; 
-        jl      _skip2         ; condition(high > 1)
-
-        xor     rbx, rbx
-        mov     ebx, edx        ; rbx = high
-        shr     ebx, 1          ; rbx = high / 2
-        add     ebx, esi        ; rbx += low
+        cmp     rsi, rdx        ; is low = size ? if so, we have exhausted our choices
+        jg      _not_found      ; return -1
         
-        cmp     ecx, DWORD [rdi + rbx*4]  ; load array[low+]
-        jl      _skip1          ; skip
+        ; CALCULATE MID
+        lea     rbx, [rdx+rsi]  ; ebx = mid, mid = low + high / 2
+        shr     rbx, 1          ; /2
 
-        lea     ebx, [ebx + 1]  ; this is eqv to
-        shr     ebx, 1          ; low += (high + 1) / 2
-        add     esi, ebx 
-
-        
-_skip1:
-        mov     rbx, rdx
-        shr     edx, 31
-        add     edx, ebx
-        shr     edx, 1
-        jmp     _search_loop
-_skip2:
-        lea     rbx, [rdi+rsi]
-        cmp     ecx, DWORD [rbx]
-        je      _found
-        mov     rax, -1
+        ; TEST where the target is: 3 cases - low, high, found
+        lea     r8, [rdi+rbx*4] ; arr[mid]
+        cmp     ecx, DWORD [r8] ; arr[mid] < target
+        je      _found          ; arr[mid] == target
+        jg      _high           ; arr[mid] > target
+        ;LOW CASE
+        lea     rdx, [rbx-1]    ; high = mid - 1
+        jmp     _search_loop    ; repeat loop
+        ;HIGH CASE
+_high:
+        lea     rsi, [rbx+1]     ; low = mid+1
+        jmp     _search_loop    ; repeat loop
+_not_found:
+        mov     rax, -1         ; when low > high, we return -1, as the target was unfound.
         jmp     end
-
 _found:
-        mov     rax, rsi        ; return index of low
+        mov     rax, rbx         ; rbx = mid, we will return this value if found
 end:
+        pop     r8
         pop     rbx
         pop     rdi
         pop     rsi
         pop     rdx
         pop     rcx
-        ret                     ; otherwise return eax, which is the current count, which equals the index.
+        ret                     
