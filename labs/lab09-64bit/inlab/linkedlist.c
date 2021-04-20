352bf42d141122a9296b4f908bbb0fe82c83465e
Joshua Tapp <joshua.c.tapp@icloud.com>
2021-04-20 15:11:28 -0400

    lab09 inlab finished
    Really simple implementation that gets the job done. This one is in pure C, so i did the implementation that is almost 1 to 1 to my C++ implemenatation.

--- labs/lab09-64bit/inlab/linkedlist.c
+++ labs/lab09-64bit/inlab/linkedlist.c
@@ -1,3 +1,11 @@
+/*
+	Joshua Tapp
+	UVA CS2150
+	4/20/2021 12:00
+	Assignment: Create a simple double linked list in pure C.
+	Notes: Used structs to allow "class-like" object oriented thinking. This allowed me to create a relatively simple program for the task.
+*/
+
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
@@ -13,7 +21,7 @@
 #define INVALID 	7
 
 #define BUFFER_SIZE 20
-
+//
 void printOptions();
 int readInput(int* val);
 int isNumber(const char* token);
@@ -22,8 +30,17 @@ int isEmpty(const char* input);
 int main(){
 
 	// TODO: INSTANTIATE YOUR LINKED LIST HERE.
+	struct list_item {
+	   int num;
+	   struct list_item *next;
+	   struct list_item *prev;
+	};
 
-
+	struct list_item *head = (struct list_item*) malloc(sizeof(struct list_item));
+	struct list_item *current = NULL;
+	struct list_item *tail = (struct list_item*) malloc(sizeof(struct list_item));
+	head->next = tail;
+	tail->prev = head;
 
 
 	//----END----
@@ -35,32 +52,50 @@ int main(){
 	do {
 		option = readInput(&val);
 		struct list_item* node;
-
 		switch(option){
 			case PUSH_FRONT:				// push onto front of list
 				// TODO: Insert code to push val onto front of linked list here.
-
-
+				node = (struct list_item*) malloc(sizeof(struct list_item));
+				node->prev = head;
+				head->next->prev = node;
+				node->next = head->next;
+				head->next = node;
+				node->num = val;
 				//----END----
 				break;
 			case PUSH_BACK: 				// push onto back of list
 				// TODO: Insert code to push val onto back of linked list here.
-				
-
+				node = (struct list_item*) malloc(sizeof(struct list_item));
+				node->next = tail;
+				tail->prev->next = node;
+				node->prev = tail->prev;
+				tail->prev = node;
+				node->num = val;
 				//----END----
 				break;
 			case POP_FRONT: 				// remove from front of list
 				// TODO: Insert code to remove from front of linked list here.
 				// If list is empty, do nothing.
 
+				if(head->next != tail) {			
+					current = head->next;
+					head->next = current->next;
+					current->next->prev = head;
+					free(current);
+				}
 
 				//----END----
 				break;
 			case POP_BACK:					// remove from back of list
 				// TODO: Insert code to remove from back of linked list here.
 				// If list is empty, do nothing.
-				
 
+				if(tail->prev != head) {
+					current = tail->prev;
+					tail->prev = current->prev;
+					current->prev->next = tail;
+					free(current);
+				}
 				//----END----
 				break;
 			case PRINT_LIST:				// print list
@@ -68,7 +103,12 @@ int main(){
 				// Simply print each element separated by a space as shown below:
 				// Elements: 1 2 3 4 5 
 				printf("Elements: ");
-
+				current = head->next;
+				while(current != tail) {
+					printf("%d ", current->num);
+					current = current->next;
+				}
+				printf("\n");
 
 				//----END----
 				break;
@@ -85,7 +125,13 @@ int main(){
 	} while(option != QUIT);
 
 	// TODO: free any memory used by your linked list here
-	
+	current = head;
+	while(current != tail) {
+		struct list_item* temp = current;
+		current = current->next;
+		free(temp); 
+	}
+		free(tail);
 
 	//----END----
 
